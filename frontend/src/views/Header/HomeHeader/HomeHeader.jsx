97b0faf11032154ef553761b5d55e33854d5dbf7
after7203 <after7203@naver.com>
2022-12-29 22:50:21 +0900

    FEAT:Add login modal

--- frontend/src/views/Header/HomeHeader/HomeHeader.jsx
+++ frontend/src/views/Header/HomeHeader/HomeHeader.jsx
@@ -3,7 +3,7 @@ import {
 } from 'react-router-dom'
 import './HomeHeader.scss'
 
-function HomeHeader() {
+function HomeHeader({toggleLoginModal}) {
     return (
         <header>
             <Link to="/"><img className="logo" src={require("../../../asset/logo_1.png")} /></Link>
@@ -17,6 +17,9 @@ function HomeHeader() {
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="search bi bi-search" viewBox="0 0 16 16">
                     <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
                 </svg>
+            </div>
+            <div className="login-btn" onClick={toggleLoginModal}>
+                로그인
             </div>
             <div className="write-btn">
                 새 글 작성
