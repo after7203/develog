dc7321c7882528c811731a7fbd5e598ba024d3c7
after7203 <after7203@naver.com>
2023-01-05 18:10:18 +0900

    FEAT: BaseURI add

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,8 +4,8 @@ import { createContext } from "react"
 import { Outlet } from "react-router-dom"
 
 export const userContext = createContext()
-//export const baseURI = process.env.NODE_ENV === 'production' ? "develog-after7203.koyeb.app/" : ""
-export const baseURI = ""
+export const baseURI = process.env.NODE_ENV === 'production' ? "develog-after7203.koyeb.app/" : ""
+//export const baseURI = ""
 
 const App = () => {
     const [user, setUser] = useState(false)
