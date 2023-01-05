2c8013ee1d9dabb72ca178edd572300ea6e4c4c5
after7203 <after7203@naver.com>
2023-01-05 20:30:37 +0900

    FEAT: BaseURI add

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,9 +1,10 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const {createProxyMiddleware} = require('http-proxy-middleware');
 
 module.exports = function(app){
   app.use(
       createProxyMiddleware('/api', {
-          target: process.env.NODE_ENV === 'production' ? "https://develog-after7203.koyeb.app:3001/" : 'http://localhost:3001/',
+        target: "https://develog-after7203.koyeb.app/",
+          //target: process.env.NODE_ENV === 'production' ? "https://develog-after7203.koyeb.app:3001/" : 'http://localhost:3001/',
           changeOrigin: true
       })
   )
