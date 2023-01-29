79233eb103407fd73ea19f5406558282aee3f4b1
after7203 <after7203@naver.com>
2023-01-29 14:40:04 +0900

    FEAT: Add post upload

--- index.js
+++ index.js
@@ -3,22 +3,24 @@ const app = express()
 const port = 3001
 const cors = require('cors');
 const bodyParser = require('body-parser')
-
 const mongodb = require("mongodb");
 const mongoose = require('mongoose');
-const {mongoURI} = require("./config/key");
-mongoose.connect(mongoURI, {dbName: 'develog'}).then("mongodb connect")
+mongoose.set('strictQuery',true)
+require("dotenv").config()
+mongoose.connect(process.env.MONGO_URI, {dbName: 'develog'}).then("mongodb connect")
 
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use('/api/users', require('./routes/users'));
+app.use('/api/board', require('./routes/board'));
+app.use('/public', express.static('public'));
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  //console.log("server on")
+  console.log("server on")
 })
